c5aa5c8a3585f0f092bfcd19192fe3df1846af42
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-08 16:30:28 -0500

    finish code challenge

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,17 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plants, search}) {
+
+  const filteredPlants = plants.filter(plant => {
+    return plant.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+  const card = filteredPlants.map(plant => {
+    return <PlantCard key={plant.id} {...plant} />
+  })
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{card}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState('')
+
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then((response) => response.json())
+    .then((data) => setPlants(data))
+  }, [])
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm
+        setPlants={setPlants}/>
+      <Search
+       search ={search}
+       setSearch={setSearch}/>
+      <PlantList
+        plants={plants}
+        search={search}
+         />
     </main>
   );
 }
